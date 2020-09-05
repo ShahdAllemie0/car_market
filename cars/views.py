8d1536cca9003b4db68f6fe07055aeb592751993
Shahd Allemie <shahdallemie@Shahds-MacBook-Pro.local>
2020-09-05 19:42:32 +0300

    done

--- cars/views.py
+++ cars/views.py
@@ -1,32 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Car
-
+from .forms import CarForm
+# Style your List and Detail views to use Bootstrap components.
+# Now that your List and Detail views look all pretty, we can probably make them look even better if each object had an image. So, add a new field to the Car model called img and make sure the field stores images and is optional.
+# Once you have the img field, make sure that the image gets displayed in the List and Detail views.
+# Next, we want to complete our CRUD features. We still need the following views:
+# Create view
+# Update view
+# Delete view
+# The views have been partially written for you, complete them. You've also been provided with the URLs, so don't worry about that!
 def car_list(request):
-	cars = Car.objects.all()
-	context = {
-		"cars": cars,
-	}
-	return render(request, 'car_list.html', context)
+    cars = Car.objects.all()
+    context = {
+    "cars": cars,
+    }
+    return render(request, 'car_list.html', context)
 
 
 def car_detail(request, car_id):
-	car = Car.objects.get(id=car_id)
-	context = {
-		"car": car,
-	}
-	return render(request, 'car_detail.html', context)
+    car = Car.objects.get(id=car_id)
+    context = {
+    "car": car,
+    }
+    return render(request, 'car_detail.html', context)
+
+
 
 
 def car_create(request):
-	#Complete Me
-	return render(...)
+    form = CarForm()
+    if request.method == "POST":
+         form = CarForm(request.POST, request.FILES)
+         if form.is_valid():
+              form.save()
+              return redirect('car-list')
+    context = {
+        "form":form,}
+    return render(request, 'car_create.html', context)
 
 
 def car_update(request, car_id):
-	#Complete Me
-	return render(...)
+    car_obj = Car.objects.get(id=car_id)
+    form = CarForm(instance=car_obj)
+    if request.method == "POST":
+          form = CarForm(request.POST, request.FILES, instance=car_obj)
+          if form.is_valid():
+                form.save()
+                return redirect('car-list')
+    context = {
+    "car_obj": car_obj,
+    "form":form,
+    }
+
+    return render(request, 'car_update.html', context)
+
 
 
 def car_delete(request, car_id):
-	#Complete Me
-	return render(...)
+    car_obj = Car.objects.get(id=car_id)
+    car_obj.delete()
+    return redirect('car-list')
